beff80a089dbad3e2d416d502f3390439a79a7fc
Jorge Rodriguez <jorge@grupostudium.com>
2019-11-27 10:10:26 +0100

    Commit inicial

---
+++ src/es/studium/Compartir/Compartir.java
@@ -0,0 +1,29 @@
+package es.studium.Compartir;
+
+public class Compartir
+{
+	int variable;
+	
+	Compartir()
+	{
+		variable = 0;
+	}
+	Compartir(int v)
+	{
+		variable = v;
+	}
+	
+	public int getVariable()
+	{
+		return variable;
+	}
+	public void setVariable(int variable)
+	{
+		this.variable = variable;
+	}
+	public static void main(String[] args)
+	{
+		Compartir objeto = new Compartir(33);
+		System.out.println(objeto.getVariable());
+	}
+}
